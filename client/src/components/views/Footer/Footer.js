679304bf7ba55657f1d6324ff9cfc3758ed452f7
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-12-03 21:07:08 +0100

    Add Basic Navbar + Footer Customization

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import styles from './Footer.module.scss';
 
 const Footer = () => (
   <footer
     id="sticky-footer"
-    className="py-4 mt-5 bg-dark text-white-50 fixed-bottom"
+    className={`py-4 mt-5 fixed-bottom ${styles.footer}`}
   >
     <div className="text-center">
-      <small>Copyright &copy; Chevret Bakery 2022</small>
+      <small>Copyright &copy; Chevret Bakery 2022. All rights reserved.</small>
     </div>
   </footer>
 );
--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from './NavBar.module.scss';
 import { IMGS_URL } from '../../../config';
 import { Navbar, Nav, Form, Button, Container } from 'react-bootstrap';
@@ -6,60 +5,56 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" className={styles.custom}>
-      <Container className="p-0">
-        <Navbar.Brand href="/" className="d-flex justify-content-center col">
-          <div className="d-flex align-items-start justify-content-center col-6 col-md-4">
+    <Navbar expand="lg" className={styles.navbar}>
+      <Container>
+        <Navbar.Brand
+          href="/"
+          className={`d-flex justify-content-start md-justify-content-center col ${styles.brand}`}
+        >
+          <div className="d-flex align-items-start justify-content-center col-4 col-md-4">
             <img
               src={IMGS_URL + 'logo.svg'}
-              width="200"
-              height="200"
               className="d-inline-block align-top"
               alt="Bakery Logo"
             />
           </div>
-          <div className="d-flex align-items-start justify-content-center flex-column col-6 col-md-8">
-            <span className="fs-md-4 fs-2"> Crunchy Dreams </span>
-            <span className="fs-md-5 fs-3"> Online Shop </span>
+          <div className="d-flex align-items-start justify-content-center flex-column col-4 col-md-8">
+            <span> Crunchy Dreams </span>
+            <span> ONLINE SHOP </span>
           </div>
         </Navbar.Brand>
 
-        <div className="d-flex col">
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse
-            id="basic-navbar-nav"
-            className="justify-content-end text-center"
-          >
-            <Nav className="fs-4 me-3">
-              <Nav.Link as={NavLink} to="/">
-                Home
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="text-center">
+          <Nav className="fs-4 me-3">
+            <Nav.Link as={NavLink} to="/">
+              Home
+            </Nav.Link>
+            <>
+              <Nav.Link as={NavLink} to="/auth/register">
+                Sign Up
               </Nav.Link>
-              <>
-                <Nav.Link as={NavLink} to="/auth/register">
-                  Sign Up
-                </Nav.Link>
-                <Nav.Link as={NavLink} to="/auth/login">
-                  Login
-                </Nav.Link>
-              </>
-            </Nav>
-            <Form className="d-flex m-2">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2 my-2"
-                aria-label="Search"
-              />
-              <Button
-                type="submit"
-                className="align-self-center"
-                variant="outline-success"
-              >
-                Search
-              </Button>
-            </Form>
-          </Navbar.Collapse>
-        </div>
+              <Nav.Link as={NavLink} to="/auth/login">
+                Login
+              </Nav.Link>
+            </>
+          </Nav>
+          <Form className="d-flex m-2">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2 my-2"
+              aria-label="Search"
+            />
+            <Button
+              type="submit"
+              className={`align-self-center ${styles.button}`}
+              variant="outline"
+            >
+              Search
+            </Button>
+          </Form>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
