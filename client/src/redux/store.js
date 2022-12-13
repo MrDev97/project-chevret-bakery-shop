6da6ce77b8f18bfbf9dd658a43ac75e7a2890371
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-12-13 23:58:26 +0100

    Turn Off Redux DevTools

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -17,9 +17,9 @@ const store = createStore(
   reducer,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : (f) => f,
+    // window.__REDUX_DEVTOOLS_EXTENSION__
+    //   ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    //   : (f) => f,
   ),
 );
 
